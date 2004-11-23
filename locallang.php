02a200408334f9c2343c3784e808b52234d9f484
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2004-11-23 04:33:52 +0000

    Initial synchronisation with latest version
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rtehtmlarea/trunk@1160 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ locallang.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Language labels for the main module of htmlArea RTE (extension: sr_htmlarea)
+ * 
+ * This file is detected by the translation tool.
+ */
+
+$LOCAL_LANG = Array (
+	'default' => Array (
+		'Please wait' => 'The editor is loading. Please wait...',
+		'Normal' => 'Normal',
+		'Heading 1' => 'Heading 1',
+		'Heading 2' => 'Heading 2',
+		'Heading 3' => 'Heading 3',
+		'Heading 4' => 'Heading 4',
+		'Heading 5' => 'Heading 5',
+		'Heading 6' => 'Heading 6',
+		'Preformatted' => 'Preformatted',
+	),
+	'dk' => Array (
+	),
+	'de' => Array (
+	),
+	'no' => Array (
+	),
+	'it' => Array (
+	),
+	'fr' => Array (
+		'Please wait' => 'L\'éditeur est en cours de chargement. Veuillez patienter...',
+		'Normal' => 'Normal',
+		'Heading 1' => 'Titre 1',
+		'Heading 2' => 'Titre 2',
+		'Heading 3' => 'Titre 3',
+		'Heading 4' => 'Titre 4',
+		'Heading 5' => 'Titre 5',
+		'Heading 6' => 'Titre 6',
+		'Preformatted' => 'Préformaté',
+	),
+	'es' => Array (
+		'Please wait' => 'El editor se está cargando. Espere por favor...',
+		'Normal' => 'Normal',
+		'Heading 1' => 'Encabezado 1',
+		'Heading 2' => 'Encabezado 2',
+		'Heading 3' => 'Encabezado 3',
+		'Heading 4' => 'Encabezado 4',
+		'Heading 5' => 'Encabezado 5',
+		'Heading 6' => 'Encabezado 6',
+		'Preformatted' => 'Preformateado',
+	),
+	'nl' => Array (
+	),
+	'cz' => Array (
+	),
+	'pl' => Array (
+	),
+	'si' => Array (
+	),
+	'fi' => Array (
+		'Normal' => 'Normaali',
+		'Heading 1' => 'Otsikko 1',
+		'Heading 2' => 'Otsikko 2',
+		'Heading 3' => 'Otsikko 3',
+		'Heading 4' => 'Otsikko 4',
+		'Heading 5' => 'Otsikko 5',
+		'Heading 6' => 'Otsikko 6',
+		'Preformatted' => 'Esimuotoiltu',
+	),
+	'tr' => Array (
+	),
+	'se' => Array (
+	),
+	'pt' => Array (
+	),
+	'ru' => Array (
+	),
+	'ro' => Array (
+	),
+	'ch' => Array (
+	),
+	'sk' => Array (
+	),
+	'lt' => Array (
+	),
+	'is' => Array (
+	),
+	'hr' => Array (
+	),
+	'hu' => Array (
+	),
+	'gl' => Array (
+	),
+	'th' => Array (
+	),
+	'gr' => Array (
+	),
+	'hk' => Array (
+	),
+	'eu' => Array (
+	),
+	'bg' => Array (
+	),
+	'br' => Array (
+	),
+	'et' => Array (
+	),
+	'ar' => Array (
+	),
+	'he' => Array (
+	),
+	'ua' => Array (
+	),
+	'lv' => Array (
+	),
+	'jp' => Array (
+	),
+	'vn' => Array (
+	),
+	'ca' => Array (
+	),
+	'ba' => Array (
+	),
+	'kr' => Array (
+	),
+);
+?>
