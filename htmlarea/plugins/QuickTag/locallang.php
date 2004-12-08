877ab0553d991cd615ef5f70b5d411c911273503
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2004-12-08 23:12:25 +0000

    QuickTag initial integration
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rtehtmlarea/trunk@1218 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ htmlarea/plugins/QuickTag/locallang.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Language labels for plugin QuickTag of extension htmlArea RTE (key: rtehtmlarea)
+ * 
+ * This file is detected by the translation tool.
+ */
+
+$LOCAL_LANG = Array (
+	'default' => Array (
+		'Quick Tag Editor' => 'Quick TAG Editor',
+		'Enter the TAG you want to insert' => 'Enter the TAG you want to insert',
+		'You have to select some text' => 'You have to select some text',
+		'There are some unclosed quote' => 'There are some unclosed quotes',
+		'This attribute already exists in the TAG' => 'This attribute already exists in the TAG',
+		'No CSS class avaiable' => 'No CSS classes available',
+		'OPTIONS' => 'OPTIONS',
+		'ATTRIBUTES' => 'ATTRIBUTES',
+		'TAGs' => 'TAGs',
+		'Colors' => 'Colors',
+		'Ok' => 'Ok',
+		'Cancel' => 'Cancel',
+	),
+	'dk' => Array (
+	),
+	'de' => Array (
+	),
+	'no' => Array (
+	),
+	'it' => Array (
+	),
+	'fr' => Array (
+		'Quick Tag Editor' => 'Éditeur de balise',
+		'Enter the TAG you want to insert' => 'Entrez la balise que vous désirez insérer.',
+		'You have to select some text' => 'Aucun texte n\'est sélectionné.',
+		'There are some unclosed quote' => 'Il y a des guillemets non fermés.',
+		'This attribute already exists in the TAG' => 'Cette balise porte déjà cet attribut.',
+		'No CSS class avaiable' => 'Aucune classe CSS disponible.',
+		'OPTIONS' => 'OPTIONS',
+		'ATTRIBUTES' => 'ATTRIBUTS',
+		'TAGs' => 'BALISES',
+		'Colors' => 'Couleurs',
+		'Ok' => 'OK',
+		'Cancel' => 'Annuler',
+	),
+	'es' => Array (
+	),
+	'nl' => Array (
+	),
+	'cz' => Array (
+	),
+	'pl' => Array (
+	),
+	'si' => Array (
+	),
+	'fi' => Array (
+	),
+	'tr' => Array (
+	),
+	'se' => Array (
+	),
+	'pt' => Array (
+	),
+	'ru' => Array (
+	),
+	'ro' => Array (
+	),
+	'ch' => Array (
+	),
+	'sk' => Array (
+	),
+	'lt' => Array (
+	),
+	'is' => Array (
+	),
+	'hr' => Array (
+	),
+	'hu' => Array (
+	),
+	'gl' => Array (
+	),
+	'th' => Array (
+	),
+	'gr' => Array (
+	),
+	'hk' => Array (
+	),
+	'eu' => Array (
+	),
+	'bg' => Array (
+	),
+	'br' => Array (
+	),
+	'et' => Array (
+	),
+	'ar' => Array (
+	),
+	'he' => Array (
+	),
+	'ua' => Array (
+	),
+	'lv' => Array (
+	),
+	'jp' => Array (
+	),
+	'vn' => Array (
+	),
+	'ca' => Array (
+	),
+	'ba' => Array (
+	),
+	'kr' => Array (
+	),
+);
+?>
---
+++ htmlarea/plugins/QuickTag/quick-tag.js
@@ -0,0 +1,57 @@
+/*---------------------------------------*\
+ Quick Tag Editor Plugin for HTMLArea-3.0
+ -----------------------------------------
+ author: Cau guanabara 
+ e-mail: caugb@ibest.com.br
+\*---------------------------------------*/
+
+QuickTag.I18N = QuickTag_langArray;
+
+function QuickTag(editor) {
+this.editor = editor;
+var cfg = editor.config;
+var self = this;
+
+cfg.registerButton("quickeditor", QuickTag.I18N["Quick Tag Editor"], 
+                   editor.imgURL("ed_quicktag.gif", "QuickTag"), false,
+                   function(editor) { self.buttonPress(editor); });
+
+  for(i = 0; i < cfg.toolbar.length; i++) {
+  var joincfg = cfg.toolbar[i].join("|"); 
+    if(/htmlmode/.test(joincfg)) {
+    cfg.toolbar[i] = joincfg.replace(/htmlmode/, "htmlmode|quickeditor").split("|");
+    var htmok = true;
+    }
+  }
+  if(!htmok) {
+  var line = cfg.toolbar[1] ? 1 : 0;
+  cfg.toolbar[line].push("separator","quickeditor");
+  }
+};
+
+QuickTag.prototype.buttonPress = function(editor) { 
+var self = this;
+var sel = editor.getSelectedHTML().replace(/(<[^>]*>|&nbsp;|\n|\r)/g,""); 
+var param = new Object();
+param.editor = editor;
+
+  if(/\w/.test(sel))
+    editor._popupDialog("plugin://QuickTag/quicktag", function(p) { self.setTag(p); }, param);
+  else
+    alert(QuickTag.I18N['You have to select some text']);
+};
+
+QuickTag.prototype.setTag = function(param) { 
+this.editor.surroundHTML(param.tagopen,param.tagclose);
+};
+
+QuickTag._pluginInfo = {
+name          : "QuickTag",
+version       : "1.0 - beta",
+developer     : "Cau Guanabara",
+developer_url : "mailto:caugb@ibest.com.br",
+c_owner       : "Cau Guanabara",
+sponsor       : "Independent production",
+sponsor_url   : "http://www.netflash.com.br/gb/HA3-rc1/examples/quick-tag.html",
+license       : "htmlArea"
+};
