2faaca6bd01fdd9ef43b1d78c1bbbecd5c4aed1a
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2004-12-12 15:56:16 +0000

    Cleaning code
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rtehtmlarea/trunk@1239 735d13b6-9817-0410-8766-e36946ffe9aa

--- t3_popup.php
+++ t3_popup.php
@@ -5,34 +5,30 @@ switch( $HTTP_GET_VARS["popupname"] ) {
 }
 $src = $HTTP_GET_VARS["srcpath"];
 ?>
-<html style="width: 500px; height: 300px">
+<html style="width: 500px; height: 300px;">
 <head>
-	<title><?php echo $title;?></title>
-
+<title><?php echo $title;?></title>
 <script type="text/javascript" src="htmlarea/popups/popup.js"></script>
 <script type="text/javascript">
-window.resizeTo(500, 300);
+	window.resizeTo(500, 300);
+	var parent = window.opener;
 
-var parent = window.opener;
+	function _CloseOnEsc() {
+		if (event.keyCode == 27) { window.close(); return; }
+	}
 
-function _CloseOnEsc() {
-  if (event.keyCode == 27) { window.close(); return; }
-}
+	function Init() {
+		i18n = window.opener.HTMLArea.I18N.dialogs; // load the HTMLArea lang file
+		__dlg_init();
+  		__dlg_translate(i18n);  // this will translate the window title
+  		document.body.onkeypress = _CloseOnEsc;
 
-function Init() {
-  i18n = window.opener.HTMLArea.I18N.dialogs; // load the HTMLArea lang file
-  __dlg_init();
-  __dlg_translate(i18n);  // this will translate the window title
-  document.body.onkeypress = _CloseOnEsc;
-
-  parent = window.opener;
-  self.parent = window.opener;
-
-  setTimeout("idPopup.focus();",3000);
-}
+		parent = window.opener;
+		self.parent = window.opener;
+		setTimeout("idPopup.focus();",3000);
+	}
 
 <?php
-
 if ($HTTP_GET_VARS["popupname"] == "image") {
 	echo '
 	setTimeout("init_selectedImageRef();",100);
@@ -43,28 +39,23 @@ if ($HTTP_GET_VARS["popupname"] == "image") {
 				setTimeout("init_selectedImageRef();",100);
 				return;
 			}
-
 			idPopup.selectedImageRef = self.parent._selectedImage;
 			idPopup.insertImagePropertiesInForm();
 		}
 	}
-
 ';
 }
-
 ?>
-
-
 </script>
 </head>
 
 <body style="background:ButtonFace; margin:0px; padding:0px; width: 500px; height: 300px; border: none;" onload="Init();">
-<table border="0px" cellspacing="0px" cellpadding="0" width="100%" HEIGHT=100%>
+<table border="0px" cellspacing="0px" cellpadding="0" width="100%" height="100%">
 <tr><td>
 <?php
 //echo $src;
 echo '
-  <IFRAME ID=idPopup name=idPopup target=idPopup WIDTH=100% HEIGHT=100% STYLE="visibility: visible;border: none;" SRC="' . $src . '?' . $HTTP_SERVER_VARS[QUERY_STRING] . '"></IFRAME>
+  <iframe id="idPopup" name="idPopup" target="idPopup" width="100%" height="100%" style="visibility: visible; border: none;" src="' . $src . '?' . $HTTP_SERVER_VARS[QUERY_STRING] . '"></iframe>
 ';
 //echo $HTTP_SERVER_VARS[QUERY_STRING];
 ?>
